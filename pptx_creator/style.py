be8f2ff524d3c5ff27d32c197fc0438a15376b27
Jules Ponsaillé <jules.ponsaille@isep.fr>
2019-10-18 12:01:48 +0200

    add basic styling

--- pptx_creator/style.py
+++ pptx_creator/style.py
@@ -1,4 +1,5 @@
 import re
+from pptx.dml.color import RGBColor
 
 def getStylingAreasPosInTextFrame(texts):
   return re.finditer(r"\/\*\*(.*?)\*\*\/", ''.join(texts))
@@ -39,6 +40,18 @@ def removeStylingAreas(paragraphs, stylingAreas):
   for paragraph in toRemove:
     paragraph._element.delete()
 
+def replaceTags(text, data): 
+  tags = [x.group(0) for x in re.finditer(r"\$\{([A-Za-z0-9._\-]+)\}", ''.join(text))]
+
+  for tag in tags:
+    replacement = data.get(tag[2:-1])
+    if not (replacement is None):
+      text = text.replace(tag, replacement)
+    else:
+      text = text.replace(tag, '')
+  
+  return text
+
 def parser(paragraphs, data):
   """
   Parse paragraphs to return all the style in a unique 
@@ -48,14 +61,48 @@ def parser(paragraphs, data):
 
   stylingAreas = [stylingArea.group(1).split(';') for stylingArea in stylingAreas]
   lines = [line.split(':') for sublist in stylingAreas for line in sublist]
-
   style = {}
 
   for line in lines:
     if(len(line) == 2):
-
-      replacement = data.get(line[1])
-      if not (replacement is None):
-        style[line[0]] = data.get(line[1])
-
+        style[line[0].strip()] = replaceTags(line[1], data)
   return style
+
+def fill(placeholder, apply):
+  """
+  Apply background color
+  """
+  if 'solid' in apply:
+    placeholder.fill.solid()
+
+  color = re.search(r'#([0-9a-fA-F]{6})', apply)
+  if color:
+    placeholder.fill.fore_color.rgb = RGBColor.from_string(color.group(0)[1:])
+
+def line(placeholder, apply):
+  """
+  Apply borders
+  """
+  color = re.search(r'#([0-9a-fA-F]{6})', apply)
+  if color:
+    placeholder.line.color.rgb = RGBColor.from_string(color.group(0)[1:])
+
+def basicFormating(placeholder, style):
+  """
+  Apply style properties commun to all placeholders
+
+  Args:
+    placeholder
+    style
+  """
+  # TODO: allow for 3 characters hex colors
+  
+  stylers = {
+    'fill': fill,
+    'line': line
+  }
+
+  for (rule, apply) in style.items():
+    if rule in stylers:
+      stylers.get(rule)(placeholder, apply)
+  
